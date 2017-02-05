12b48abe848a3a930af023f4fd00fff818353fa0
Luan Freitas <luan.lrf@gmail.com>
2017-02-04 21:34:25 -0300

    Alterando config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $conn = 'mysql:host=localhost;dbname=pbxip';
 try {
-    $db = new PDO($conn, 'softium', 'softium@123');
+    $db = new PDO($conn, 'pbxip', 'pbxip@123');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     if($e->getCode() == 1049){
